1ab7e39007235782c35b75cbe2f71457da60afd0
yangyanning <yangyanning@sensetime.com>
2024-03-20 12:56:45 +0800

    initial commit

---
+++ sum.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_DEPTH 128
+
+void foo(int depth, int lo, int hi, int s, int t, int* prev) {
+    int i, j, k;
+    if (depth == 0) {
+        if (s == t) {
+            i = 0;
+            while (prev[i] > 0)
+                printf("%d, ", prev[i++]);
+            printf("\n");
+        }
+        return;
+    }
+
+    j = 0;
+    while (prev[j] > 0)
+        ++j;
+    for (i = lo; i <= hi; ++i) {
+        prev[j] = i;
+        k = j + 1;
+        while (prev[k] > 0)
+            prev[k++] = 0;
+        foo(depth - 1, i + 1, hi, s + i, t, &prev[0]);
+    }
+    return;
+}
+
+
+int main(int argc, char* argv[]) {
+    int prev[MAX_DEPTH] = {0};
+    int depth = 4, t = 12;
+    if (argc > 1) {
+        depth = atoi(argv[1]);
+        t = atoi(argv[2]);
+    }
+	printf("depth = %d, target = %d\n", depth, t);
+    foo(depth, 1, 2 * depth, 0, t, &prev[0]);
+    return 0;
+}
---
+++ sum5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	int t = 20;
+	const int lo = 1, hi = 10;
+	int k1, k2, k3, k4, k5;
+	if (argc > 1) {
+		t = atoi(argv[1]);
+	}
+	printf("target = %d\n", t);
+	for (k1 = lo; k1 <= hi; k1++) {
+		for (k2 = k1 + 1; k2 <= hi; k2++) {
+			for (k3 = k2 + 1; k3 <= hi; k3++) {
+				for (k4 = k3 + 1; k4 <= hi; k4++) {
+					for (k5 = k4 + 1; k5 <= hi; k5++) {
+						if (k1 + k2 + k3 + k4 + k5 == t) {
+							printf("%d, %d, %d, %d, %d\n", k1, k2, k3, k4, k5);
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
